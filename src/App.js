8be66d3eaa097aa7dd01ead7c8aaa3d60cf98683
Phạm Tấn Minh Tiến <phamtanminhtien@gmail.com>
2022-10-20 11:45:07 +0700

    test ci-cd

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Thanh loz
-        </p>
+        <p>Test ci-cd</p>
+        <p>Thanh Lozzzzzzzzz 😬</p>
         <a
           className="App-link"
           href="https://reactjs.org"
