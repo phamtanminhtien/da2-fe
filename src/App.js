1160fa386589d8e111ee5c700cb7161d4db9e325
Phạm Tấn Minh Tiến <phamtanminhtien@gmail.com>
2022-12-04 11:55:47 +0700

    test ci-cd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Test ci-cd</p>
-        <p>Thanh Lozzzzzzzzz 😬</p>
+        <p>Thanh Lozzzzzzzzz</p>
         <a
           className="App-link"
           href="https://reactjs.org"
