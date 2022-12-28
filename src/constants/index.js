660870c695b673ff71cdb924027196129c4bc4b5
Doan Khiet Thanh <doankhietthanh@gmail.com>
2022-12-28 22:50:41 +0700

    update endpoint

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,3 @@
 // export const baseURL = "https://be.uilabs.store";
-export const baseURL = "http://localhost:4000";
+// export const baseURL = "http://localhost:4000";
+export const baseURL = "http://be.giamgialab.com";
